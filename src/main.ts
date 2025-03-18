6e83238d850406a19815919efadb6a7a2eaa54eb
Pedro Ollero <pedroollero1998@gmail.com>
2025-03-18 09:38:48 +0100

    Add game functionality and UI updates for card game

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,94 @@
-import "./style.css";
+let card: number = 0;
+let puntuation: number = 0;
+let puntuationBound: number = 0;
 
-console.log("Hello Typescript!");
+const getRandomNum = (): number => {
+  let randomNum: number = Math.floor((Math.random() * 9) + 1);
+  if (randomNum > 7) {
+    return (puntuation = 0.5);
+  } else {
+    return (puntuation = randomNum);
+  }
+};
+
+const gameHandler = (puntuation: number) => {
+  if (puntuation < 4) {
+    console.log("Has sido muy conservador");
+  } else if (puntuation >= 5 && puntuation <= 7.5) {
+    switch (puntuation) {
+      case 5:
+        console.log("Te ha entrado el canguelo eh");
+        break;
+      case 6:
+        console.log("Casi casi...");
+        break;
+      case 7:
+        console.log("Casi casi...");
+        break;
+      case 7.5:
+        console.log("¡Lo has clavado! ¡Enhorabuena!");
+        break;
+    }
+  }
+};
+
+
+const puntuationBoundHandler = (newNumber: number): number => {
+  return (puntuationBound = puntuationBound + newNumber);
+};
+
+const hitMeHandler = () => {
+  const hitMeButton = document.getElementById("pedir");
+  if (hitMeButton instanceof HTMLButtonElement) {
+    hitMeButton.addEventListener("click", () => {
+      puntuationBoundHandler(getRandomNum());
+      showPuntuation(puntuationBound);
+      gameOver(puntuationBound)
+    });
+  }
+};
+
+const standHandler = () => {
+  const standButton = document.getElementById("plantar");
+  if (standButton instanceof HTMLButtonElement) {
+    standButton.addEventListener("click", () => {
+      gameHandler(puntuationBound);
+    });
+  }
+};
+
+const resetHandler = () => {
+  let resetButton = document.getElementById("reset");
+  if (resetButton instanceof HTMLButtonElement) {
+    resetButton.addEventListener("click", () => {
+      puntuationBound = 0;
+      showPuntuation(puntuationBound);
+      gameOver(0)
+    });
+  }
+};
+
+const showPuntuation = (impressPuntuation: number) => {
+  let impressPuntuationString: string = impressPuntuation.toString();
+
+  let showPuntuationScreen = document.getElementById("puntuacion__numero");
+
+  if (showPuntuationScreen instanceof HTMLElement) {
+    showPuntuationScreen.textContent = impressPuntuationString;
+  }
+};
+
+const gameOver = (puntuacion: number) => {
+    const gameOverScreen = document.querySelector(".gameover")!;
+    if(gameOverScreen instanceof HTMLParagraphElement && puntuacion > 7.5){
+        gameOverScreen.classList.add("active");
+    }else{
+        gameOverScreen.classList.remove("active");
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  hitMeHandler();
+  standHandler();
+  resetHandler();
+});
