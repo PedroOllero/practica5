e252ed02a22b33bfedc70bd5c5c775582996ff6c
Pedro Ollero <pedroollero1998@gmail.com>
2025-03-21 12:47:16 +0100

    Refactor game logic and UI handling for improved readability and functionality

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,12 @@
 let randomNum: number = 0;
 let puntuationBound: number = 0;
-
 const setPuntuationBound = (value: number): number => {
   return (puntuationBound = value);
 };
 
-const getRandomNum = (): number => {
-  randomNum = Math.floor(Math.random() * 10 + 1);
-  if (randomNum > 7) {
-    return randomNum + 2;
-  }
-  return randomNum;
-};
+const getRandomNum = (): number => Math.floor(Math.random() * 10) + 1;
+
+const addTwo = (num: number): number => (num > 7 ? num + 2 : num);
 
 const getValueHandler = (points: number): number => {
   if (points > 7) {
@@ -21,81 +16,84 @@ const getValueHandler = (points: number): number => {
   }
 };
 
-const getCardHandler = (puntuation: number) => {
+const getCardImage = (puntuacion?: string): string => {
+  switch (puntuacion) {
+    case "0":
+      return "assets/img/back.jpg";
+    case "1":
+      return "assets/img/1_as-copas.jpg";
+    case "2":
+      return "assets/img/2_dos-copas.jpg";
+    case "3":
+      return "assets/img/3_tres-copas.jpg";
+    case "4":
+      return "assets/img/4_cuatro-copas.jpg";
+    case "5":
+      return "assets/img/5_cinco-copas.jpg";
+    case "6":
+      return "assets/img/6_seis-copas.jpg";
+    case "7":
+      return "assets/img/7_siete-copas.jpg";
+    case "10":
+      return "assets/img/10_sota-copas.jpg";
+    case "11":
+      return "assets/img/11_caballo-copas.jpg";
+    case "12":
+      return "assets/img/12_rey-copas.jpg";
+    default:
+      return "assets/img/back.jpg";
+  }
+};
+
+const paintCard = (puntuacion?: string): void => {
+  const cardImage = getCardImage(puntuacion);
   const card = document.getElementById("carta");
-  if (card instanceof HTMLImageElement) {
-    switch (puntuation) {
-      case 0:
-        card.src = "";
-        break;
-      case 1:
-        card.src = "assets/img/1_as-copas.jpg";
-        break;
-      case 2:
-        card.src = "assets/img/2_dos-copas.jpg";
-        break;
-      case 3:
-        card.src = "assets/img/3_tres-copas.jpg";
-        break;
-      case 4:
-        card.src = "assets/img/4_cuatro-copas.jpg";
-        break;
-      case 5:
-        card.src = "assets/img/5_cinco-copas.jpg";
-        break;
-      case 6:
-        card.src = "assets/img/6_seis-copas.jpg";
-        break;
-      case 7:
-        card.src = "assets/img/7_siete-copas.jpg";
-        break;
-      case 10:
-        card.src = "assets/img/10_sota-copas.jpg";
-        break;
-      case 11:
-        card.src = "assets/img/11_caballo-copas.jpg";
-        break;
-      case 12:
-        card.src = "assets/img/12_rey-copas.jpg";
-        break;
-      default:
-        card.src = "assets/img/back.jpg";
-        break;
+  if (card && card instanceof HTMLImageElement) {
+    card.src = cardImage;
+  }
+};
+
+const commentTransition = (activo: boolean) => {
+  const comment = document.querySelector(".comentario");
+  if (comment && comment instanceof HTMLParagraphElement) {
+    if (activo) {
+      comment.classList.add("active");
+    } else {
+      comment.classList.remove("active");
     }
-  } else {
-    console.log("algo va mal");
   }
 };
 
 const gameHandler = (puntuation: number) => {
   const comment = document.querySelector(".comentario");
-  if (comment instanceof HTMLParagraphElement) {
+  commentTransition(true);
+  if (comment && comment instanceof HTMLParagraphElement) {
     if (puntuation < 4 && puntuation > 0) {
       comment.textContent = "Has sido muy conservador";
-      comment.classList.add("active");
     } else if (puntuation >= 5 && puntuation <= 7.5) {
       switch (puntuation) {
         case 5:
           comment.textContent = "Te ha entrado el canguelo eh";
-          comment.classList.add("active");
+          break;
+        case 5.5:
+          comment.textContent = "Te ha entrado el canguelo eh";
           break;
         case 6:
           comment.textContent = "Casi casi...";
-          comment.classList.add("active");
+          break;
+        case 6.5:
+          comment.textContent = "Casi casi...";
           break;
         case 7:
           comment.textContent = "Casi casi...";
-          comment.classList.add("active");
           break;
         case 7.5:
           comment.textContent = "¡Lo has clavado!";
-          comment.classList.add("ganador");
           break;
         default:
       }
     } else {
-      comment.textContent = "";
-      comment.classList.remove("active");
+      commentTransition(false);
     }
   }
 };
@@ -107,37 +105,50 @@ const puntuationBoundHandler = (newNumber: number): number => {
 };
 
 const hitMeHandler = () => {
+  const randomNum: number = getRandomNum();
+  const cartNumb: number = addTwo(randomNum);
+  const value: number = getValueHandler(cartNumb);
+  puntuationBoundHandler(value);
+  showPuntuation(puntuationBound);
+  gameOver(puntuationBound);
+  paintCard(cartNumb.toString());
+};
+
+const hitMeButtonHandler = () => {
   const hitMeButton = document.getElementById("pedir");
-  if (hitMeButton instanceof HTMLButtonElement) {
+  if (hitMeButton && hitMeButton instanceof HTMLButtonElement) {
     hitMeButton.addEventListener("click", () => {
-      const randomNum = getRandomNum();
-      const value = getValueHandler(randomNum);
-      puntuationBoundHandler(value);
-      showPuntuation(puntuationBound);
-      gameOver(puntuationBound);
-      getCardHandler(randomNum);
+      hitMeHandler();
     });
   }
 };
 
 const standHandler = () => {
+  gameHandler(puntuationBound)
+};
+
+const standButtonHandler = () => {
   const standButton = document.getElementById("plantar");
-  if (standButton instanceof HTMLButtonElement) {
+  if (standButton && standButton instanceof HTMLButtonElement) {
     standButton.addEventListener("click", () => {
-      gameHandler(puntuationBound);
+      standHandler();
     });
   }
 };
 
 const resetHandler = () => {
+  setPuntuationBound(0);
+  showPuntuation(puntuationBound);
+  gameOver(0);
+  paintCard("0");
+  gameHandler(0);
+}
+
+const resetButtonHandler = () => {
   let resetButton = document.getElementById("reset");
-  if (resetButton instanceof HTMLButtonElement) {
+  if (resetButton && resetButton instanceof HTMLButtonElement) {
     resetButton.addEventListener("click", () => {
-      setPuntuationBound(0);
-      showPuntuation(puntuationBound);
-      gameOver(0);
-      getCardHandler(0);
-      gameHandler(0);
+      resetHandler();
     });
   }
 };
@@ -147,22 +158,30 @@ const showPuntuation = (impressPuntuation: number) => {
 
   let showPuntuationScreen = document.getElementById("puntuacion__numero");
 
-  if (showPuntuationScreen instanceof HTMLElement) {
+  if (showPuntuationScreen && showPuntuationScreen instanceof HTMLElement) {
     showPuntuationScreen.textContent = impressPuntuationString;
   }
 };
 
 const gameOver = (puntuacion: number) => {
   const gameOverScreen = document.querySelector(".gameover")!;
-  if (gameOverScreen instanceof HTMLParagraphElement && puntuacion > 7.5) {
+  if (
+    gameOverScreen &&
+    gameOverScreen instanceof HTMLParagraphElement &&
+    puntuacion > 7.5
+  ) {
     gameOverScreen.classList.add("active");
   } else {
     gameOverScreen.classList.remove("active");
   }
 };
 
+const buttonsHandler = () => {
+  hitMeButtonHandler();
+  standButtonHandler();
+  resetButtonHandler();
+};
+
 document.addEventListener("DOMContentLoaded", () => {
-  hitMeHandler();
-  standHandler();
-  resetHandler();
+  buttonsHandler();
 });
