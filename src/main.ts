49defbd94b17c94982658e7dfdf03f84d366c86a
Pedro Ollero <pedroollero1998@gmail.com>
2025-03-24 16:07:47 +0100

    Add winning message for high score in gameHandler logic

--- src/main.ts
+++ src/main.ts
@@ -77,6 +77,9 @@ const gameHandler = (puntuation: number): string => {
     return "Casi casi...";
   }
 
+  if (puntuation > 7.5) {
+    return "¡HAS GANADO!";
+  }
 
   if (puntuation > 7.5) {
     return "Te has pasado...";
